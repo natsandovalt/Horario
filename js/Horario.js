b6cf73de17ff4457a4746a7cb6597b4c03c0ee5c
ssandoval95 <santiago_st_95@hotmail.com>
2015-03-21 15:29:38 -0600

    Added materias information

--- js/Horario.js
+++ js/Horario.js
@@ -21,3 +21,227 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+/*Programación*/
+var Materia = {
+  nomMateria: "Programación Orientada Objetos",
+  horaInicio: "09:00",
+  horaFin:"10:30",
+  diasClase: [
+  false,
+  true,
+  false,
+  false,
+  true
+  ],
+  nombreProfesor: "Luis Humberto González",
+  claveMateria: "TC2016.3",
+  salon: "A3303"
+};
+/*Organización Computacional*/
+var Materia = {
+  nomMateria: "Organización Computacional",
+  horaFin: "10:30",
+  horaFin: "12:00",
+  diasClase: [
+  false,
+  true,
+  false,
+  false,
+  true
+  ],
+  nombreProfesor: "Mario Isidro de la Fuente",
+  claveMateria: "TC1016.3",
+  salon: "A3101"
+};
+/*Diseño Interactivo*/
+var Materia = {
+  nomMateria: "Fundamentos de Diseño Interactivo",
+  horaInicio: "15:00",
+  horaFin: "16:30",
+  diasClase: [
+  false,
+  true,
+  false,
+  false,
+  true
+  ],
+  nombreProfesor: "Cleopatra Garza",
+  claveMateria: "TC1015.1",
+  salon: "CI220B"
+};
+/*Administración*/
+var Materia = {
+  nomMateria :"Administración e innovación en modelos de negocio",
+  horaInicio: "09:00",
+  horaFin: "10:30",
+  diasClase: [
+  true,
+  false,
+  false,
+  true,
+  false
+  ],
+  nombreProfesor: "Miguel Angel Flores",
+  claveMateria: "AD1005.3",
+  salon: "A6404"
+};
+/*Matemáticas*/
+var Materia = {
+  nomMateria: "Matemáticas II",
+  horaInicio: "10:30",
+  horaFin: "12:00",
+  diasClase: [
+  true,
+  false,
+  false,
+  true,
+  false
+  ],
+  nombreProfesor: "Juan Antonio Alanís",
+  claveMateria: "MA1017.3",
+  salon: "A4307"
+};
+
+var Materia = {
+  nomMateria: "Matemáticas II",
+  horaInicio: "12:00",
+  horaFin: "13:30",
+  diasClase: [
+  true,
+  false,
+  false,
+  true,
+  false
+  ],
+  nombreProfesor: "María Dhlema Rendón",
+  claveMateria: "MA1017.28",
+  salon: "A7403"
+};
+
+var Materia = {
+  nomMateria: "Matemáticas I",
+  horaInicio: "12:00",
+  horaFin: "13:30",
+  diasClase: [
+  true,
+  false,
+  false,
+  true,
+  false
+  ],
+  nombreProfesor: "Francisco Morales",
+  claveMateria: "Ma1015.29",
+  salon: "A7205"
+};
+/*Análisis y expresión verbal*/
+var Materia = {
+  nomMateria: "Análisis y expresión verbal",
+  horaInicio: "15:00",
+  horaFin: "16:30",
+  diasClase: [
+  true,
+  false,
+  true,
+  true,
+  false
+  ],
+  nombreProfesor: "Mirthala García",
+  claveMateria: "H1040.18",
+  salon: "A1416"
+};
+/*Cambio Climático*/
+var Materia = {
+  nomMateria: "Cambio climático y uso de energía",
+  horaInicio: "15:00",
+  horaFin: "16:30",
+  diasClase: [
+  false,
+  true,
+  false,
+  false,
+  true
+  ],
+  nombreProfesor: "Ana Yael Vanoye",
+  claveMateria: "DS1005.15",
+  salon: "A4420"
+};
+/*Física*/
+var Materia = {
+  nomMateria: "Física I",
+  horaInicio: "13:30",
+  horaFin: "15:00",
+  diasClase: [
+  true,
+  false,
+  false,
+  true,
+  false
+  ],
+  nombreProfesor: "Oscar Arturo Garza",
+  claveMateria: "F1002.12",
+  salon: "CI406"
+};
+/*Alemán*/
+var Materia = {
+  nomMateria: "Almán básico II",
+  horaInicio: "13:30",
+  horaFin: "15:00",
+  diasClase: [
+  true,
+  false,
+  true,
+  true,
+  false
+  ],
+  nombreProfesor: "Petra Kosíkova",
+  claveMateria: "HI95021.1",
+  salon: "A4207"
+};
+/*Matemáticas Discretas*/
+var Materia = {
+  nomMateria: "Matemáticas Discretas",
+  horaInicio: "15:00",
+  horaFin: "16:30",
+  diasClase: [
+  true,
+  false,
+  false,
+  true,
+  false
+  ],
+  nombreProfesor: "Ramón Felipe Brena",
+  claveMateria:"TC1003.5",
+  salon: "A3302"
+};
+/*Fundamentos de Programación*/
+var Materia = {
+  nomMateria: "Fundamentos de Programación",
+  horaInicio: "10:30",
+  horaFin: "12:00",
+  diasClase [
+  false,
+  true,
+  false,
+  false,
+  true
+  ],
+  nombreProfesor: "Javier Alberto Giese",
+  claveMateria: "TC1014.10",
+  salon: "CDES115"
+};
+/*Introducción a la ingeniería en tecnologías computacionales*/
+var Materia = {
+  nomMateria: "Introducción a la ingeniería en tecnologías computacionales",
+  horaInicio: "13:30",
+  horaFin: "15:00",
+  diasClase [
+  false,
+  true,
+  false,
+  false,
+  true
+  ],
+  nombreProfesor: "Elda Quiroga",
+  claveMateria: "TC1022.2",
+  salon: "A6206"
+};
