934110202b4f2ae2bac121513436ece71a247019
Irvel Nduva <irvel@irvel.com>
2015-03-21 18:50:14 -0600

    Code for retrieving current subjects from html code is now working

--- js/procesar.js
+++ js/procesar.js
@@ -2,14 +2,21 @@
 
 //Decode html contents from the email body 
 function decodeHtml(html){
-	var splitted = html.split();
+	//Make an array with all subjects + extra stuff in the html code
+	var splitter = "<td colspan=3D\"10\"><b><code> ";
+	var splitted = html.split(splitter);
+	var subjects = new Array();
+	var temp;
+	for (var i = 1; i < splitted.length; i++) {
+		temp = splitted[i].split(" ", 1);
+		subjects.push(temp[0]);
+	};
 	console.log(html);
-	console.log(splitted);
+	console.log(subjects);
 }
+
 
 function callDecode(){
 	//For debugging purposes
-	decodeHtml("<td colspan=3D\"10\"><b><code> hola probando");
+	decodeHtml("<td colspan=3D\"10\"><b><code> hola probando askjdfkjafnjl asdjhfkahsdfkj <td colspan=3D\"10\"><b><code> esta es otra prueba");
 }
-
-var sContenido = "algo";
